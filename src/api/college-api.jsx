48844f4bd541a023da43636e314de0842d0820fc
sachinuttirnaservice <sachinuttirnaservice@gmail.com>
2025-02-26 14:19:52 +0530

    cleanup

--- src/api/college-api.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://192.168.1.17:5000",
-});
-
-export const createCollege = (data) => {
-  return api.post("/api/create/", data);
-};
